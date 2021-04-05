0486c3744f14dc5fd64cf68a5a651664add3b1a0
Surdu <neon-doubt@maskmail.net>
2021-04-06 00:44:39 +0300

    Implemented weather

---
+++ src/Dashboard/Weather/Weather.jsx
@@ -0,0 +1,76 @@
+import {useEffect, useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTint, faUmbrella, faWind, faCompass } from '@fortawesome/free-solid-svg-icons'
+import {formatDistanceToNow} from 'date-fns';
+import roLocale from 'date-fns/locale/ro';
+
+import styles from './Weather.module.scss';
+
+import * as weatherApi from '../../api/weather';
+import {TIME} from '../../utils/time';
+import {degToCompass} from '../../utils/compass';
+import {Widget} from '../Widget/Widget';
+
+export function Weather() {
+  const [measurement, setMeasurement] = useState({});
+
+  function fetchWeather() {
+    weatherApi.getLatest().then(function(response) {
+      console.log("response:", response);
+      setMeasurement(response);
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+  }
+
+  useEffect(function init() {
+    fetchWeather();
+    const fetchInterval = setInterval(fetchWeather, 1 * TIME.MINUTES);
+
+    return function() {
+      clearInterval(fetchInterval);
+    }
+  }, []);
+
+  return (
+    <Widget title="Vremea" className={styles.weather}>
+      {measurement &&
+        <>
+          <div className={styles.temperature}>
+            {measurement.temp?.toString().replace("." , ",")} <span>˚C</span>
+          </div>
+          <table className={styles.values}>
+            <tbody>
+              <tr>
+                <td><FontAwesomeIcon icon={faTint}/></td>
+                <th>Umiditate:</th>
+                <td>{measurement.humidity}%</td>
+              </tr>
+              <tr>
+                <td><FontAwesomeIcon icon={faUmbrella}/></td>
+                <th>Precipitații:</th>
+                <td>{measurement.rain} mm</td>
+              </tr>
+              <tr>
+                <td><FontAwesomeIcon icon={faWind}/></td>
+                <th>Vânt:</th>
+                <td>{measurement.windspeed?.toFixed(2)} km/h</td>
+              </tr>
+              <tr>
+                <td><FontAwesomeIcon icon={faCompass}/></td>
+                <th>Direcție vânt:</th>
+                <td>{degToCompass(measurement.winddir)}</td>
+              </tr>
+            </tbody>
+          </table>
+          <div className={styles.footer}>
+            Ultima actualizare: {measurement.time && formatDistanceToNow(measurement.time, {
+          addSuffix: true,
+          locale: roLocale
+        })}</div>
+        </>
+      }
+    </Widget>
+  )
+}
---
+++ src/utils/compass.js
@@ -0,0 +1,5 @@
+export function degToCompass(num) {
+  var val = Math.floor((num / 22.5) + 0.5);
+  var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+  return arr[(val % 16)];
+}
